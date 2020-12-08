724a688ae4395d88f68d33b6197332c0c25d8782
Mason Hawver <mason.hawver@gmail.com>
2020-12-08 15:30:36 -0600

    Update Chess.java

--- BoardGames/Chess.java
+++ BoardGames/Chess.java
@@ -1,18 +1,36 @@
 
-/**
- * Write a description of class Chess here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+import java.util.*;
+
+
 public class Chess extends Game 
 {
+    static final boolean light = false;
+    static final boolean dark = true;
+    
     public Chess() {
         super("Chess");
-    }
-    
-    public void init() {
+        light_pieces = new LinkedList<Piece>();
+        dark_pieces = new LinkedList<Piece>();
+        
         board = new ChessBoard(this);
         add(board);
+    }
+    
+    public void addPiece(Piece p) {
+        if (p.color == light) { light_pieces.add(p); }
+        else { dark_pieces.add(p); }
+    }
+    
+    public void removePiece(Piece p) {
+        if (p.color == light) { light_pieces.remove(p); }
+        else { dark_pieces.remove(p); }
+    }
+    
+    public LinkedList<Piece> dark_pieces;
+    public LinkedList<Piece> light_pieces;
+    
+    public static void main(String[] args) {
+        Game chess = new Chess();
+        chess.play();
     }
 }
