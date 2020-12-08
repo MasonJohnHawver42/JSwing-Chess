56d4404af34d0ba02c9fcc95158dcf4b53aba6ce
Mason Hawver <mason.hawver@gmail.com>
2020-12-08 10:25:05 -0600

    Add files via upload

---
+++ BoardGames/BoardGame/Tile.java
@@ -0,0 +1,73 @@
+package BoardGame;
+
+import BoardGame.Board;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+abstract public class Tile<B extends Board> extends JButton {
+
+  public Tile(B b, int r, int c) {
+      board = b;
+      row = r; col = c;
+
+      state = initState();
+      state.start();
+  }
+
+  abstract protected State initState();
+
+  public final void setState(State new_state) {
+      State old_state = state;
+
+      old_state.terminate();
+
+      if (!new_state.terminated) {
+        state = new_state;
+         new_state.start();
+      }
+  }
+
+  //getters
+
+  public <T extends Tile<B>> LinkedList<T> getNeighbors() {
+    LinkedList<T> n = new LinkedList<T>();
+
+    for (int i = -1; i <= 1; i++) {
+      for (int j = -1; j <= 1; j++) {
+          if (i != 0 || j != 0) {
+            try { n.add((T)board.getTile(row + i, col + j)); }
+            catch(RuntimeException e) { }
+          }
+      }
+    }
+
+    return n;
+  }
+
+  protected B board;
+
+  protected State state;
+
+  protected int row, col;
+
+  public String toString() {
+    return "Tile: [" + Integer.toString(row) + " , " + Integer.toString(col) + "]";
+  }
+
+  abstract public class State<T extends Tile> implements ActionListener {
+    public State(T t) { tile = t; terminated = false; }
+
+    public final void actionPerformed(ActionEvent e) { update(e); }
+
+    public void start() { tile.addActionListener(this); }
+    abstract public void update(ActionEvent e);
+    public void terminate() { tile.removeActionListener(this); terminated = true; }
+
+    protected T tile;
+
+    boolean terminated;
+  }
+}
---
+++ BoardGames/MineSweeper.java
@@ -0,0 +1,141 @@
+
+import BoardGame.*;
+
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+///
+/// Game
+///
+
+
+public class MineSweeper extends Game
+{
+    public MineSweeper() {
+        super("Mine Sweeper");
+    }
+
+    protected void init() {
+        board = new MSBoard(this, 10);
+        add(board);
+    }
+
+    ///
+    /// Board
+    ///
+
+    private class MSBoard extends Board<MineSweeper> {
+        public MSBoard(MineSweeper g, int size) {
+            super(g, size, size);
+        }
+
+        @Override
+        public Tile initTile(int i, int j) { return new MSTile(this, i, j); }
+    }
+
+    ///
+    /// Cell
+    ///
+
+    private class MSTile extends Tile<MSBoard> {
+
+        public MSTile(MSBoard b, int r, int c) {
+            super(b, r, c);
+        }
+
+        @Override
+        public Tile.State initState() { return new UnInit(this); }
+
+        private Boolean mined;
+
+        ///
+        /// States
+        ///
+
+        // State UnInit
+
+        private class UnInit extends State<MSTile> {
+            public UnInit(MSTile t) {
+                super(t);
+                t.setText("U");
+            }
+            public void update(ActionEvent e) {
+
+                tile.mined = false;
+
+                tile.state = new UnChecked(tile);
+                tile.state.start();
+
+                LinkedList<MSTile> ns = tile.getNeighbors();
+                for (MSTile n: ns) { n.mined = false; n.setState(new UnChecked(n)); }
+
+                tile.setState(new Checked(tile));
+
+            }
+            public void start() { super.start(); }
+            public void terminate() { super.terminate(); }
+        }
+
+        // State UnChecked
+
+        private class UnChecked extends State<MSTile> {
+            public UnChecked(MSTile t) {
+                super(t);
+                t.setText(" ");
+            }
+            public void start() {
+              super.start();
+
+              LinkedList<MSTile> ns = tile.getNeighbors();
+              for (MSTile n: ns) {
+                if (n.state instanceof UnInit) {
+                  n.mined = Math.random() > .8;
+                  n.setState(new UnChecked(n));
+                }
+              }
+            }
+            public void update(ActionEvent e) {
+              tile.setState(new Checked(tile));
+            }
+            public void terminate() { super.terminate(); }
+        }
+
+        // State Checked
+
+        private class Checked extends State<MSTile> {
+            public Checked(MSTile t) {
+                super(t);
+            }
+            public void start() {
+              super.start();
+
+              String label = "B";
+
+              if (!tile.mined) {
+                int bombs = 0;
+                LinkedList<MSTile> ns = tile.getNeighbors();
+                for (MSTile n : ns) { bombs += n.mined ? 1 : 0; }
+                if (bombs == 0) {
+                  for (MSTile n : ns) {
+                    if (n.state instanceof UnChecked) { n.setState(new Checked(n)); }
+                  }
+                }
+
+                label = Integer.toString(bombs);
+              }
+
+              tile.setText(label);
+
+            }
+            public void update(ActionEvent e) { }
+            public void terminate() { super.terminate(); }
+          }
+        }
+
+    public static void main(String[] args) {
+        Game ms = new MineSweeper();
+        ms.play();
+    }
+}
