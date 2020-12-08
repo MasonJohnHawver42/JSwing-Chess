cc41e925f58d36c01170eb97f05efaff51abc57f
Mason Hawver <mason.hawver@gmail.com>
2020-12-08 15:57:38 -0600

    Update Pawn.java

--- BoardGames/Pawn.java
+++ BoardGames/Pawn.java
@@ -1,6 +1,12 @@
-
+import java.util.*;
 
 public class Pawn extends Piece
 {
-
+    public Pawn(Boolean color) {
+        super(color);
+        name = "P";
+    }
+    
+    protected LinkedList<ChessTile> moves(ChessTile tile) { return null; }
 }
+
