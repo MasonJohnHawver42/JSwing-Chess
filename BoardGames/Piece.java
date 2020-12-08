626c298476c9943713d543e7cc187826b8270213
beachjosh <75635331+beachjosh@users.noreply.github.com>
2020-12-08 11:43:45 -0600

    Update Piece.java

--- BoardGames/Piece.java
+++ BoardGames/Piece.java
@@ -1,6 +1,16 @@
-
-
-abstract public class Piece
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+abstract public class Piece<B extends Board> extends JButton
 {
+    public Piece(B board, int x, int y, Boolean b)
+    {
+        if(b==true)
+            board.getTile(x,y).setBackground(Color.BLACK);
+        else
+            board.getTile(x,y).setBackground(Color.WHITE);
+    }
     
+    abstract protected Board moves(int x, int y);
 }
