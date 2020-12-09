44cf1b27fd22c8cbd264a320880cb53a8e4c6355
Whitetiger094 <75635340+Whitetiger094@users.noreply.github.com>
2020-12-09 11:24:35 -0600

    Update ChessBoard.java

--- BoardGames/ChessBoard.java
+++ BoardGames/ChessBoard.java
@@ -14,14 +14,16 @@ public class ChessBoard extends Board<Chess>
       placePiece(new Pawn(Chess.dark), 1, 4);
       placePiece(new Pawn(Chess.dark), 1, 5);
       placePiece(new Pawn(Chess.dark), 1, 6);
-      placePiece(new Pawn(Chess.dark), 1, 7);/*
+      placePiece(new Pawn(Chess.dark), 1, 7);
+      placePiece(new Rook(Chess.dark), 0, 0);
+      placePiece(new Rook(Chess.dark), 0, 7);
       placePiece(new Knight(Chess.dark), 0, 1);
       placePiece(new Knight(Chess.dark), 0, 6);
       placePiece(new Bishop(Chess.dark), 0, 2);
       placePiece(new Bishop(Chess.dark), 0, 5);
       placePiece(new Queen(Chess.dark), 0, 3);
       placePiece(new King(Chess.dark), 0, 4);
-      */
+      
       placePiece(new Pawn(Chess.light), 6, 0);
       placePiece(new Pawn(Chess.light), 6, 1);
       placePiece(new Pawn(Chess.light), 6, 2);
@@ -29,13 +31,15 @@ public class ChessBoard extends Board<Chess>
       placePiece(new Pawn(Chess.light), 6, 4);
       placePiece(new Pawn(Chess.light), 6, 5);
       placePiece(new Pawn(Chess.light), 6, 6);
-      placePiece(new Pawn(Chess.light), 6, 7);/*
-      placePiece(new Knight(Chess.dark), 7, 1);
-      placePiece(new Knight(Chess.dark), 7, 6);
-      placePiece(new Bishop(Chess.dark), 7, 2);
-      placePiece(new Bishop(Chess.dark), 7, 5);
-      placePiece(new Queen(Chess.dark), 7, 3);
-      placePiece(new King(Chess.dark), 7, 4);*/
+      placePiece(new Pawn(Chess.light), 6, 7);
+      placePiece(new Rook(Chess.light), 7, 7);
+      placePiece(new Rook(Chess.light), 7, 0);
+      placePiece(new Knight(Chess.light), 7, 1);
+      placePiece(new Knight(Chess.light), 7, 6);
+      placePiece(new Bishop(Chess.light), 7, 2);
+      placePiece(new Bishop(Chess.light), 7, 5);
+      placePiece(new Queen(Chess.light), 7, 3);
+      placePiece(new King(Chess.light), 7, 4);
     }
     
     public Tile initTile(int i, int j) 
