f643aebaff9db976ecdf18b9a316c2c113f39c75
Mason Hawver <mason.hawver@gmail.com>
2020-12-09 10:57:34 -0600

    Update Queen.java

--- BoardGames/Queen.java
+++ BoardGames/Queen.java
@@ -1,11 +1,24 @@
 
-/**
- * Write a description of class Queen here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+import java.util.*;
+
+//mason
+
 public class Queen extends Piece
 {
+        public Queen(Boolean color) {
+        super(color);
+        name = "P";
+    }
     
+    protected LinkedList<ChessTile> moves(ChessTile tile) { 
+        LinkedList<ChessTile> moves = new LinkedList<ChessTile>();
+        
+        Bishop bish = new Bishop(this.color);
+        Rook rook = new Rook(this.color);
+        
+        moves.addAll(bish.moves(tile));
+        moves.addAll(rook.moves(tile));
+        
+        return moves;
+    }
 }
