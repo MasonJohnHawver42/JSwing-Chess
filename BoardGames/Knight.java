0ab5472a0514858524765adbffb6c3c232c11947
Whitetiger094 <75635340+Whitetiger094@users.noreply.github.com>
2020-12-09 11:23:42 -0600

    Update Knight.java

--- BoardGames/Knight.java
+++ BoardGames/Knight.java
@@ -1,11 +1,19 @@
+package BoardGames;
 
-/**
- * Write a description of class Knight here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+import java.util.*;
+
 public class Knight extends Piece
 {
+  public Knight(Boolean color) 
+  {
+        super(color);
+        name = "Kn";
+  }
     
+   protected LinkedList<ChessTile> moves(ChessTile tile) 
+   { 
+      LinkedList<ChessTile> moves = new LinkedList<ChessTile>();
+        
+      return moves;
+  } 
 }
