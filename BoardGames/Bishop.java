451b4b14355cdc1993d5ac5a83d64d1cf4504f5f
beachjosh <75635331+beachjosh@users.noreply.github.com>
2020-12-10 11:49:52 -0600

    Update Bishop.java

--- BoardGames/Bishop.java
+++ BoardGames/Bishop.java
@@ -8,11 +8,63 @@ public class Bishop extends Piece
         super(color);
         name = "B";
     }
-    
+    //Not finished, will work on tonight to try and fix - Josh
     protected LinkedList<ChessTile> moves(ChessTile tile) 
     { 
       LinkedList<ChessTile> moves = new LinkedList<ChessTile>();
-        
+      ChessTile move;
+      int x=-1;
+      try{
+      move = tile.getNeighbor(-1,-1);
+      
+      while(move.empty()){
+          
+          moves.add(move);
+          x--;
+          move = tile.getNeighbor(color ? 1:x,x);
+        }
+      }
+      catch(RuntimeException e){}
+      x=1;
+      try{
+      move = tile.getNeighbor(1,1);
+      
+      while(move.empty()){
+          
+          moves.add(move);
+          x++;
+          move = tile.getNeighbor(color ? 1:x,x);
+        }
+      }
+      catch(RuntimeException e){}
+      x=1;
+      
+      try{
+      move = tile.getNeighbor(1,-1);
+      
+      while(move.empty()){
+          
+          moves.add(move);
+          x++;
+          
+          move = tile.getNeighbor(color ? 1:x,-x);
+        }
+      }
+      catch(RuntimeException e){}
+      x=-1;
+      try{
+      move = tile.getNeighbor(-1,1);
+      
+      while(move.empty()){
+          
+          moves.add(move);
+          x--;
+          
+          move = tile.getNeighbor(color ? 1:x,-x);
+        }
+      }
+      catch(RuntimeException e){}
+      
       return moves;
     }
 }
