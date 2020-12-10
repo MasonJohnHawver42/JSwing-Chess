926ce0074061d396f0bbce0f6f0a44bd1e181f6c
Mason Hawver <mason.hawver@gmail.com>
2020-12-10 13:34:57 -0600

    Update Knight.java

--- BoardGames/Knight.java
+++ BoardGames/Knight.java
@@ -12,7 +12,29 @@ public class Knight extends Piece
     
    protected LinkedList<ChessTile> moves(ChessTile tile) 
    { 
-      LinkedList<ChessTile> moves = new LinkedList<ChessTile>();
+      int[] offsets =  { 2, -1,
+                         2,  1,
+                        -2,  1,
+                        -2, -1,
+                         1,  2,
+                        -1,  2,
+                         1, -2,
+                        -1, -2 };
+      
+      for (int i = 0; i < offsets.length; i += 2 ) {
+        try {
+            ChessTile move = tile.getNeighbor(offsets[i], offsets[i + 1]);
+            
+            if (!move.empty()) {
+                Piece other = move.getPiece();
+                boolean other_color = other.getColor();
+                if (other_color != color ) { moves.add(move); }
+            }
+            else {
+                moves.add(move);
+            }
+        } catch(RuntimeException e) {}
+      }
         
       return moves;
   } 
