fa1fa7e0741c057141449aac17e1aa2d889da8ca
Mason Hawver <mason.hawver@gmail.com>
2020-12-09 19:06:53 -0600

    Update Chess.java

--- BoardGames/Chess.java
+++ BoardGames/Chess.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Chess extends Game 
 {
-    static final boolean light = false;
+    static final boolean light = false; 
     static final boolean dark = true;
     
     public Chess() {
@@ -12,9 +12,16 @@ public class Chess extends Game
         light_pieces = new LinkedList<Piece>();
         dark_pieces = new LinkedList<Piece>();
         
+        turn = light;
+    }
+    
+    public void init() {
         board = new ChessBoard(this);
         add(board);
     }
+    
+    public void select(Tile t) { selected = t; }
+    public Tile getSelected() { return selected; }
     
     public void addPiece(Piece p) {
         if (p.color == light) { light_pieces.add(p); }
@@ -26,8 +33,13 @@ public class Chess extends Game
         else { dark_pieces.remove(p); }
     }
     
-    public LinkedList<Piece> dark_pieces;
-    public LinkedList<Piece> light_pieces;
+    private LinkedList<Piece> dark_pieces;
+    private LinkedList<Piece> light_pieces;
+    
+    boolean turn;
+    
+    Tile selected;
+    
     
     public static void main(String[] args) {
         Game chess = new Chess();
