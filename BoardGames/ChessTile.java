a5c79c2ca384b16337c17ea1c87092776af8210e
Mason Hawver <mason.hawver@gmail.com>
2020-12-10 07:48:34 -0600

    Update ChessTile.java

--- BoardGames/ChessTile.java
+++ BoardGames/ChessTile.java
@@ -16,7 +16,7 @@ public class ChessTile extends Tile<ChessBoard>
         state.start();
         
         setText(" ");
-        setPreferredSize(new Dimension(50, 50));
+        setPreferredSize(new Dimension(60, 60));
     }
     
     public boolean empty() { return piece == null; }
