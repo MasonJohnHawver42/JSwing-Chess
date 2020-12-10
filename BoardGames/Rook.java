5b03fad20d6e675d38336dc04e674814a1428f5b
Whitetiger094 <75635340+Whitetiger094@users.noreply.github.com>
2020-12-10 11:51:56 -0600

    Update Rook.java

--- BoardGames/Rook.java
+++ BoardGames/Rook.java
@@ -13,7 +13,51 @@ public class Rook extends Piece
     protected LinkedList<ChessTile> moves(ChessTile tile) 
     { 
       LinkedList<ChessTile> moves = new LinkedList<ChessTile>();
+      try 
+      {
+        ChessTile moveu = tile.getNeighbor (-1,0);
+        //ChessTile moved = tile.getNeighbor(color ? 1: 1,0);
+        //ChessTile movel = tile.getNeighbor(color ? 1: 0,-1);
+        //ChessTile mover = tile.getNeighbor(color ? 1: 0,1);
+        while(moveu.empty())  
+        {
+          moves.add(moveu);
+          moveu = tile.getNeighbor(-1, 0); 
+          Piece other = moveu.getPiece();
+          moveu = moveu.getNeighbor(-1, 0);
+          System.out.println(moveu);
+        }
         
+         
+         
+        //while(!movel.empty())  
+        //{
+        //   Piece other = movel.getPiece();
+        //  boolean other_color = other.getColor();
+        //  if (other_color != color /* our color */) 
+        //  {
+        
+        //  }
+        //  else
+        //  {
+        //    moves.add(movel);   
+        //  }
+        //}         
+        //while(!mover.empty())  
+        //{
+        //  Piece other = mover.getPiece();
+        //  boolean other_color = other.getColor();
+        //  if (other_color != color /* our color */) 
+        //  {
+
+        //  }
+        //  else
+        //  {
+        //    moves.add(mover);   
+        //  }
+        //}
+        
+      }catch(RuntimeException e) {}
       return moves;
     }
 }
