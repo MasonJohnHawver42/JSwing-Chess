6b4a0ef39a3f370226bd3d4bd84c303955ffece1
Whitetiger094 <75635340+Whitetiger094@users.noreply.github.com>
2020-12-11 11:59:21 -0600

    Update King.java
    Doesn't work quite yet but just posting what I have at the moment for King. We can talk about it next week

--- BoardGames/King.java
+++ BoardGames/King.java
@@ -10,7 +10,87 @@ public class King extends Piece
     }
     
     protected LinkedList<ChessTile> moves(ChessTile tile) 
-    { 
-      return tile.getNeighbors();
+    {
+      LinkedList<ChessTile> moves = new LinkedList<ChessTile>();  
+      try 
+      {
+            ChessTile moveu = tile.getNeighbor(-1,0);
+            if (!moveu.empty()) 
+            {  
+              moves.add(moveu);
+              moveu = moveu.getNeighbor(-1, 0); 
+            }
+      } catch(RuntimeException e) {}
+      
+      try 
+      {
+            ChessTile moved = tile.getNeighbor(1,0);
+            if (!moved.empty()) 
+            {  
+              moves.add(moved);
+              moved = moved.getNeighbor(1, 0); 
+            }
+      } catch(RuntimeException e) {}
+      
+      try 
+      {
+            ChessTile mover = tile.getNeighbor(0,1);
+            if (!mover.empty()) 
+            {  
+              moves.add(mover);
+              mover = mover.getNeighbor(0, 1); 
+            }
+      } catch(RuntimeException e) {}
+      
+      try 
+      {
+            ChessTile movel = tile.getNeighbor(0,-1);
+            if (!movel.empty()) 
+            {  
+              moves.add(movel);
+              movel = movel.getNeighbor(0, -1); 
+            }
+      } catch(RuntimeException e) {}
+      
+      try 
+      {
+            ChessTile moveul = tile.getNeighbor(-1,-1);
+            if (!moveul.empty()) 
+            {  
+              moves.add(moveul);
+              moveul = moveul.getNeighbor(-1, -1); 
+            }
+      } catch(RuntimeException e) {}
+      
+      try 
+      {
+            ChessTile moveur = tile.getNeighbor(-1,1);
+            if (!moveur.empty()) 
+            {  
+              moves.add(moveur);
+              moveur = moveur.getNeighbor(-1, 0); 
+            }
+      } catch(RuntimeException e) {}
+      
+      try 
+      {
+            ChessTile movedl = tile.getNeighbor(1,-1);
+            if (!movedl.empty()) 
+            {  
+              moves.add(movedl);
+              movedl = movedl.getNeighbor(1, -1); 
+            }
+      } catch(RuntimeException e) {}
+      
+      try 
+      {
+            ChessTile movedr = tile.getNeighbor(1,1);
+            if (!movedr.empty()) 
+            {  
+              moves.add(movedr);
+              movedr = movedr.getNeighbor(1, 1); 
+            }
+      } catch(RuntimeException e) {}
+      return moves;
     }
 }
