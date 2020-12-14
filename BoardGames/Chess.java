20883aafd6442eff203d2cd2066703f89fcb51f0
beachjosh <75635331+beachjosh@users.noreply.github.com>
2020-12-14 11:46:46 -0600

    Update Chess.java

--- BoardGames/Chess.java
+++ BoardGames/Chess.java
@@ -1,11 +1,14 @@
-
+import java.awt.*;
 import java.util.*;
-
+import javax.swing.*;
 
 public class Chess extends Game 
 {
     static final boolean light = false; 
     static final boolean dark = true;
+    static int whitePoint, blackPoint;
+    static JLabel wPoint, bPoint;
+    
     
     public Chess() {
         super("Chess");
@@ -29,8 +32,16 @@ public class Chess extends Game
     }
     
     public void removePiece(Piece p) {
-        if (p.color == light) { light_pieces.remove(p); }
-        else { dark_pieces.remove(p); }
+        if (p.color == light) { 
+            light_pieces.remove(p); 
+            blackPoint += p.getValue();
+            bPoint.setText("Black Points: " + Integer.toString(blackPoint));
+        }
+        else { 
+            dark_pieces.remove(p); 
+            whitePoint += p.getValue(); 
+            wPoint.setText("White Points: " + Integer.toString(whitePoint));
+        }
     }
     
     private LinkedList<Piece> dark_pieces;
@@ -43,6 +54,23 @@ public class Chess extends Game
     
     public static void main(String[] args) {
         Game chess = new Chess();
+        JPanel points = new JPanel();
+        
+        chess.setLayout(new BorderLayout());
+        wPoint = new JLabel();
+        JLabel spacer = new JLabel();
+        bPoint = new JLabel();
+        
+        wPoint.setText("White Points: " + Integer.toString(whitePoint));
+        spacer.setText("|\t|");
+        bPoint.setText("Black Points: " + Integer.toString(blackPoint));
+        
+        points.add(wPoint);
+        points.add(spacer);
+        points.add(bPoint);
+        
+        chess.add(points, BorderLayout.NORTH);
+        
         chess.play();
     }
 }
