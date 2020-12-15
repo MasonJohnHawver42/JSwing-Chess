c96430d83548898a29639fc24d2507875b579334
Whitetiger094 <75635340+Whitetiger094@users.noreply.github.com>
2020-12-15 11:29:16 -0600

    Update King.java

--- BoardGames/King.java
+++ BoardGames/King.java
@@ -5,9 +5,9 @@ public class King extends Piece
 {
     public King(Boolean color) 
     {
-    //I just put 1000 because I need a value there, but obvisouly if you get king you win <- ha!
-        super(color, 1000);
-        name = "K";
+     //I just put 1000 because I need a value there, but obvisouly if you get king you win <- ha!
+     super(color, 1000);
+     name = "K";
     }
     
     protected LinkedList<ChessTile> moves() 
@@ -23,6 +23,8 @@ public class King extends Piece
               moves.add(moveu);
               moveu = moveu.getNeighbor(-1, 0); 
             }
+            if(moveu.getPiece()!=null&&moveu.getPiece().getColor()!=tile.getPiece().getColor())
+                moves.add(moveu);
       } catch(RuntimeException e) {}
       
       try 
@@ -33,6 +35,8 @@ public class King extends Piece
               moves.add(moved);
               moved = moved.getNeighbor(1, 0); 
             }
+            if(moved.getPiece()!=null&&moved.getPiece().getColor()!=tile.getPiece().getColor())
+                moves.add(moved);
       } catch(RuntimeException e) {}
       
       try 
@@ -43,6 +47,8 @@ public class King extends Piece
               moves.add(mover);
               mover = mover.getNeighbor(0, 1); 
             }
+            if(mover.getPiece()!=null&&mover.getPiece().getColor()!=tile.getPiece().getColor())
+                moves.add(mover);
       } catch(RuntimeException e) {}
       
       try 
@@ -53,6 +59,8 @@ public class King extends Piece
               moves.add(movel);
               movel = movel.getNeighbor(0, -1); 
             }
+            if(movel.getPiece()!=null&&movel.getPiece().getColor()!=tile.getPiece().getColor())
+                moves.add(movel);
       } catch(RuntimeException e) {}
       
       try 
@@ -63,6 +71,8 @@ public class King extends Piece
               moves.add(moveul);
               moveul = moveul.getNeighbor(-1, -1); 
             }
+            if(moveul.getPiece()!=null&&moveul.getPiece().getColor()!=tile.getPiece().getColor())
+                moves.add(moveul);
       } catch(RuntimeException e) {}
       
       try 
@@ -73,6 +83,8 @@ public class King extends Piece
               moves.add(moveur);
               moveur = moveur.getNeighbor(-1, 0); 
             }
+            if(moveur.getPiece()!=null&&moveur.getPiece().getColor()!=tile.getPiece().getColor())
+                moves.add(moveur);
       } catch(RuntimeException e) {}
       
       try 
@@ -83,6 +95,8 @@ public class King extends Piece
               moves.add(movedl);
               movedl = movedl.getNeighbor(1, -1); 
             }
+            if(movedl.getPiece()!=null&&movedl.getPiece().getColor()!=tile.getPiece().getColor())
+                moves.add(movedl);
       } catch(RuntimeException e) {}
       
       try 
@@ -93,6 +107,8 @@ public class King extends Piece
               moves.add(movedr);
               movedr = movedr.getNeighbor(1, 1); 
             }
+            if(movedr.getPiece()!=null&&movedr.getPiece().getColor()!=tile.getPiece().getColor())
+                moves.add(movedr);
       } catch(RuntimeException e) {}
       return moves;
     }
