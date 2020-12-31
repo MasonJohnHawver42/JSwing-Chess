f1a8a2998a4aa1f6bfd0fabbd06a98485eca5151
Whitetiger094 <75635340+Whitetiger094@users.noreply.github.com>
2020-12-31 15:20:15 -0600

    Update Castle.java

--- BoardGames/Castle.java
+++ BoardGames/Castle.java
@@ -1,11 +1,27 @@
+package BoardGames;
 
-/**
- * Write a description of class Castle here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Castle
+public class Castle extends Move
 {
-    
+
+   public Castle(King k, Rook r, ChessTile t)
+   {
+        super(k, t); 
+        movek = new NormalMove(k, t);
+        mover = new NormalMove(r, k.getTile());
+   }
+   
+   private NormalMove movek;
+   private NormalMove mover;
+
+   public void Do() 
+   {
+     mover.Do();
+     movek.Do();
+   }
+
+   public void Undo() 
+   {
+     movek.Undo();
+     mover.Undo();
+   }
 }
