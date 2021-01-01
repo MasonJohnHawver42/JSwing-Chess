99c1d311a55d23b2884bd3ae1db2da3454f79e94
Whitetiger094 <75635340+Whitetiger094@users.noreply.github.com>
2021-01-01 17:37:02 -0600

    Update Castle.java

--- BoardGames/Castle.java
+++ BoardGames/Castle.java
@@ -3,20 +3,23 @@ package BoardGames;
 public class Castle extends Move
 {
 
-   public Castle(King k, Rook r, ChessTile t)
+   public Castle(King k, Rook r, ChessTile t1, ChessTile t2)
    {
-        super(k, t); 
-        movek = new NormalMove(k, t);
+        super(k, t1); 
+        movek = new NormalMove(k, t1);
         mover = new NormalMove(r, k.getTile());
+        moverr = new NormalMove(r,t2);
    }
    
    private NormalMove movek;
    private NormalMove mover;
+   private NormalMove moverr;
 
    public void Do() 
    {
      mover.Do();
      movek.Do();
+     moverr.Do();
    }
 
    public void Undo() 
